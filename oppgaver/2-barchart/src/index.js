d2738e686cc85cc88d67fa6199d8fdb9a5cce539
Svein Petter Gjøby <sveinpg@users.noreply.github.com>
2018-10-03 17:16:45 +0200

    Update index.js

--- oppgaver/2-barchart/src/index.js
+++ oppgaver/2-barchart/src/index.js
@@ -1,15 +1,14 @@
 const data = [5, 10, 1, 3, 2, 1, 4, 2, 7];
 
-const svg = document.getElementById("svg");
 const dataMax = d3.max(data);
 const width = 500;
 const height = 500;
 
-d3.select(svg).style("background-color", "#d1d1d1");
+d3.select("#svg").style("background-color", "#d1d1d1");
 
 // Legger til et rect for hvert element i data
 const graph = d3
-  .select(svg)
+  .select("#svg")
   .selectAll("rect")
   .data(data)
   .enter()
